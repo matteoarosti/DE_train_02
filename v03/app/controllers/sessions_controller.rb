aca44b482061d53f98856e73456f6a573bdc0cda
Matteo Arosti <matteo.arosti@gmail.com>
2017-06-16 18:18:23 +0200

    bug sessions (speaker_id)

--- v03/app/controllers/sessions_controller.rb
+++ v03/app/controllers/sessions_controller.rb
@@ -69,6 +69,6 @@ class SessionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def session_params
-      params.require(:session).permit(:topic, :description, :event_id, :speaker)
+      params.require(:session).permit(:topic, :description, :event_id, :speaker_id)
     end
 end
